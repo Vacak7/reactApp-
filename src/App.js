d3362e96b834cba3eb2cf80bccf1c48a00c32030
Vacak7 <lb78@outlook.com>
2018-06-19 15:11:36 +0100

    new paraghraph added

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ class App extends Component {
     return (
       <div className="App">
        <h1>Hi, I am your first react App</h1>
+       <p> This is really working </p>
       </div>
     );
+    // return React.createElement('div',null,React.createElement('h1', {ClassName: App}, 'Does this work now'));
   }
 }
 
