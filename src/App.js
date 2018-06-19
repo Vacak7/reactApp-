a8d4e90a27b82858a841a97648e638e81e200dec
Vacak7 <lb78@outlook.com>
2018-06-19 14:05:43 +0100

    new_commented

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+//Test -->
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <h1>Hi, I am your first react App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
